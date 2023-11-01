cf5fa0038086ab1abf57413315039d18a5076805
Salinaasf <131354564+Salinaasf@users.noreply.github.com>
2023-11-01 19:19:34 +0000

    Added first Spring Boot Project

---
+++ hello/src/main/java/edu/fra/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package edu.fra.uas.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloWorldController {
+    
+    @RequestMapping(value="Hello", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return "<hl>Hello World Spring Boot!</hl>";
+    }
+    
+}
